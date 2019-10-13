14c37fa8598067cd8c1f5f7cde833c4c6461866e
Alisha <43160608+unleeshthepower@users.noreply.github.com>
2019-10-13 10:47:50 -0700

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=anxiety-bot-inmyhead-Web-Deploy.pubxml -p:Password=EGhAGMfBCfwcYJfH3mvMot9Mehn1ClQowvJy6Y1mux3q6MaGfwgoKqn2rk7k
+
